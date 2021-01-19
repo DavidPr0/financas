675934089157fa6571edf90a8ae970bb9b39f61a
DavidPr0 <davidpaco2.4@gmail.com>
2021-01-19 11:21:12 -0300

    adding category model

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,13 +1,14 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 
+import { Category } from './pages/categories/shared/category.model';
 export class InMemoryDatabase implements InMemoryDbService {
   createDb() {
-    const categories = [
+    const categories: Category[] = [
       { id: 1, name: 'Moradia', description: 'Pagamentos de contas da Casa.' },
       { id: 2, name: 'Saúde', description: 'Plano de Saúde e Remédios.' },
       { id: 3, name: 'Lazer', description: 'Cinema, Parques, Praia, Etc.' },
       { id: 4, name: 'Salários', description: 'Recebimento de Salário.' },
-      { id: 5, name: 'Freelas', description: 'Trabalhos como Freelancer.' },
+      { id: 5, name: 'Freelas', description: 'Trabalhos como Freelancer.' }
     ];
     return categories;
   }
